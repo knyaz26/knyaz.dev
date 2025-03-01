3c646ba6b602122315cdb62b826fa8032acfdd47
knyaz <saba2006.diasamidze@gmail.com>
2025-03-02 00:04:09 +0400

    set up main.go file and added home page with html and basic css

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "html/template"
+    "net/http"
+)
+
+/*
+MANUAL ON HOW TO USE THIS THINGIE:
+-declare a variable of a template.
+-set up a function.
+-parse the html in main function.
+-set up a hander function in main.
+*/
+
+//declare variables here:
+var(
+    homeTemplate *template.Template
+)
+
+//set up functions here:
+func Home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    if err := homeTemplate.Execute(w, nil); err != nil {
+        panic(err)
+    }
+}
+
+func main() {
+    //parse html in here:
+    //make sure to pass in all the parameters includeing named templates.
+    homeTemplate, _ = template.ParseFiles("templates/home.html")
+
+    //set up handler function here:
+    //pass in a URL adress and function name.
+    http.HandleFunc("/", Home)
+
+    //misc:
+    http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+    http.ListenAndServe(":10000", nil)
+}
+
