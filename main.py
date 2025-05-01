ab4cb0400da335adaa7a88d70c831477327f8785
saba diasamidze <saba2006.diasamidze@gmail.com>
2025-05-01 16:41:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
+    return
